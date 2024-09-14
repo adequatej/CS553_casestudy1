7dab929247a38b858cb362cb319dda742a9e7d8b
Jed Geoghegan <jedgeoghegan@gmail.com>
2024-09-14 16:18:20 -0400

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -2,18 +2,18 @@ import pytest
 from unittest.mock import patch
 from app import spotify_rec
 
-# Mocking the Spotify API calls
+# Mocking the Spotify API calls because don't want to rely on real Spotify API responses
 @patch("spotipy.Spotify.search")
 @patch("spotipy.Spotify.recommendations")
 def test_spotify_rec(mock_recommendations, mock_search):
-    # Define mock search result
+    # Define search results
     mock_search.return_value = {
         'tracks': {
             'items': [{'uri': 'spotify:track:123', 'name': 'Shape of You', 'artists': [{'name': 'Ed Sheeran'}]}]
         }
     }
 
-    # Define mock recommendations result
+    # Define rec results
     mock_recommendations.return_value = {
         'tracks': [
             {'name': 'Song 1', 'artists': [{'name': 'Artist 1'}]},
@@ -21,10 +21,9 @@ def test_spotify_rec(mock_recommendations, mock_search):
         ]
     }
 
-    # Call the function
     result = spotify_rec("Shape of You", "Ed Sheeran", "your_client_id", "your_client_secret")
 
-    # Check that the result is not empty and is a string
+    # Check result
     assert isinstance(result, str)
     assert len(result) > 0
 
