ab05438c7a5500626b57e2937b855af6ae8a519a
Jed Geoghegan <jedgeoghegan@gmail.com>
2024-10-05 19:55:54 -0400

    Create connect.sh

---
+++ connect.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+SSH_KEY=~/CS553/Case_study_2/my_key
+PORT=22004
+MACHINE=paffenroth-23.dyn.wpi.edu
+
+ssh -i ${SSH_KEY} -p ${PORT} -o StrictHostKeyChecking=no student-admin@${MACHINE}
+
