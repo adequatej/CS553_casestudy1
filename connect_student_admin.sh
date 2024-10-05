c8ea4598728ae299a13c0f8559eeacf64d29fd55
adequatej <jgeoghegan@jgeoghegan24>
2024-10-05 19:40:07 -0400

    Updated connect_student_admin.sh with new changes

--- connect_student_admin.sh
+++ connect_student_admin.sh
@@ -3,4 +3,4 @@
 PORT=22004
 MACHINE=paffenroth-23.dyn.wpi.edu
 
-ssh -i ~/CS553/Case_study_2/student-admin_key -p ${PORT} -o StrictHostKeyChecking=no student-admin@${MACHINE}
+ssh -i ~/CS553/Case_study_2/my_key -p ${PORT} -o StrictHostKeyChecking=no student-admin@${MACHINE}
