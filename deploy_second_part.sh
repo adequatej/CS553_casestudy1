1ef8bba86b581d2c9754cf01ff597f4bff451a41
Jed Geoghegan <jedgeoghegan@gmail.com>
2024-10-05 23:47:41 -0400

    Update deploy_second_part.sh

--- deploy_second_part.sh
+++ deploy_second_part.sh
@@ -12,16 +12,8 @@ ssh-add mykey
 
 # check that the code in installed and start up the product
 COMMAND="ssh -i mykey -p ${PORT} -o StrictHostKeyChecking=no student-admin@${MACHINE}"
-${COMMAND} "ls ~/CS553/Case_study_1/Chatbot2"
+${COMMAND} "ls ~/CS553_casestudy1"
 ${COMMAND} "sudo apt install -qq -y python3-venv"
-${COMMAND} "cd ~/CS553/Case_study_1/Chatbot2 && python3 -m venv venv"
-${COMMAND} "cd ~/CS553/Case_study_1/Chatbot2 && source venv/bin/activate && pip install -r requirements.txt"
-${COMMAND} "nohup ~/CS553/Case_study_1/Chatbot2/venv/bin/python3 ~/CS553/Case_study_1/Chatbot2/app.py > log.txt 2>&1 &"
-
-# nohup ./whatever > /dev/null 2>&1 
-
-# debugging ideas
-# sudo apt-get install gh
-# gh auth login
-# requests.exceptions.HTTPError: 429 Client Error: Too Many Requests for url: https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta/v1/chat/completions
-# log.txt
+${COMMAND} "cd ~/CS553_casestudy1 && python3 -m venv venv"
+${COMMAND} "cd ~/CS553_casestudy1 && source venv/bin/activate && pip install -r requirements.txt"
+${COMMAND} "nohup ~/CS553_casestudy1/venv/bin/python3 ~/CS553_casestudy1/app.py > log.txt 2>&1 &"
