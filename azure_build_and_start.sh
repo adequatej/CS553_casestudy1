5cc8e329e2694cc766e22e47f3eee0e7cb7cb14e
Jed Geoghegan <jedgeoghegan@gmail.com>
2024-12-02 14:03:13 -0500

    Update azure_build_and_start.sh to personalize script

--- azure_build_and_start.sh
+++ azure_build_and_start.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
 # Build the container
-docker build -t rpaff/cs553_example_azure .
+docker build -t adequatej/spotifychatbot .
 
 # Push the container to docker hub
-docker push rpaff/cs553_example_azure
+docker push adequatej/spotifychatbot
 
 # Run the container on Azure
 az containerapp create \
-  --name cs553-cli-example-1 \
-  --resource-group rcpaffenroth-rg \
-  --environment managedenvironment-rcpaffenroth-rg \
-  --image rpaff/cs553_example_azure \
+  --name cs553azure \
+  --resource-group WPI-asc-export \
+  --environment managedEnvironment-WPIascexport-9633 \
+  --image adequatej/spotifychatbot \
   --ingress external \
   --target-port 7860
 
 # Get the URL
 az containerapp show \
-  --name cs553-cli-example-1 \
-  --resource-group rcpaffenroth-rg \
+  --name cs553azure \
+  --resource-group WPI-asc-export \
   --query properties.configuration.ingress.fqdn
